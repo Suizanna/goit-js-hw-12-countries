59fa6067cde8d0f36a3ca9632f79e26f5d689ed5
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-05-24 18:07:04 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -6,6 +6,13 @@ function fetchCountries(countryName) {
 
 export default { fetchCountries }; 
 
+// // async await 
+// async function fetchCountries(countryName) {
+//   const responce = await fetch(`${BASE_URL}/${countryName}`);
+//   const country = await responce..json());
+//   return country;
+// }
+
 
 // function  fetchCountries(countryName) {
 //   return fetch(`${BASE_URL}/${countryName}`).then(response => {
