f770df8528d6a6da7451a831121ab2aec32765ee
Suizanna <73129581+Suizanna@users.noreply.github.com>
2021-06-03 20:12:21 +0300

    add

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,31 +1,14 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name';
 
-function fetchCountries(countryName) {
-  return fetch(`${BASE_URL}/${countryName}`).then(response => response.json());
+export default function fetchCountries(searchQuery) {
+  return fetch(`${BASE_URL}/${searchQuery}`).then(response => response.json());
 }
 
-export default { fetchCountries }; 
-
 // // async await 
-// async function fetchCountries(countryName) {
-//   const responce = await fetch(`${BASE_URL}/${countryName}`);
-//   const country = await responce.json());
-//   return country;
+// async function fetchCountries(searchQuery)) {
+//   const responce = await fetch(`${BASE_URL}/${searchQuery)}`);
+//   return await responce.json());
 // }
 
 
-// function  fetchCountries(countryName) {
-//   return fetch(`${BASE_URL}/${countryName}`).then(response => {
-//     if (response.ok) {
-//       return response.json();
-//     }
-
-//     throw new Error();
-//   })
-//     .catch(error => {
-//       // console.log(error)
-//       errorNotification('No matches found. Try again.');
-//       return error;
-//     });
-// }
-// export default { fetchCountries }; 
+ 
--- src/js/search.js
+++ src/js/search.js
@@ -1,54 +1,47 @@
+import fetchCountrie from './fetchCountries.js';
 import debounce from 'lodash.debounce';
-import fetchAPI from './fetchCountries.js';
-import listOfContriesTpl from '../templates/list-of-countries.hbs';
-import countryCardTpl from '../templates/country-markup.hbs';
-
+import listOfContriesTpl from '../templates/list.hbs';
+import createCountryMarkup from '../templates/country.hbs';
 import { tooManyCountries, noResult } from './notify.js';
 
+const ref = {
+    input: document.querySelector('.input-search'),
+    list: document.querySelector('.country-list'),
+}
 
-const inputRef = document.querySelector('.input-search');
-const cardContainer = document.querySelector('.js-card-container');
+ref.input.addEventListener('input', debounce(onSearch, 500),);
 
-inputRef.addEventListener('input', debounce(onSearch, 500),);
-
-
-function onSearch() {
-  // clearMarkup();
-
-  const query = inputRef.value; //запрос значения импута
-  if (!query.trim()) {
-    return;
-  //или
-  // let inputValue = inputRef.value.replace(/[^a-z]/gi, '');
-  // let query = '';    
-  // query = inputValue;  //const queryValue = event.target.value;
-  }
+function onSearch(e) {
   
-  fetchAPI.fetchCountries(query)
-    .then(checkingNumberOfCountries)
-    .catch(noResult);
+    if (e.target.value.trim() !== '') { 
+    fetchCountrie(e.target.value.trim()).then(data => {
+        console.log(data);
+        if (data.length > 1  && data.length < 11) {
+        ref.list.innerHTML = listOfContriesTpl(data);
+        return;
+        }
+        if(data.length === 1) {
+            ref.list.innerHTML = createCountryMarkup(data[0]);
+            return;
+        }
+        if (data.length > 10) {
+            tooManyCountries();
+            return;
+        }
+        if (data.status === 404) {
+            error({
+                text: 'Введите корректный запрос',
+                type: 'error',
+                delay: 2000,
+            })
+            return;
+        }
+    });
 }
-
-function checkingNumberOfCountries(data) { 
-  if (data.length > 10) {
-    tooManyCountries();
-  } else if (data.length >= 2  && data.length <= 10) {
-    renderMarkup(listOfContriesTpl, data);
-  } else if (data.length === 1) {
-    renderMarkup(countryCardTpl, data[0]);
-  } else {
-    noResult();
-  }
 }
-
-function renderMarkup(template, data) { 
-  const markup = template(data);
-  // cardContainer.insertAdjacentHTML('beforeend', markup);
-  cardContainer.inserttHTML('beforeend', markup);
-}
-
-function clearMarkup() {
-  cardContainer.innerHTML = '';
-}
-
-
+//деструктур
+// function onSearch({ target: {value} }) {
+//     if (value.trim() !== '') { 
+//   fetchCountrie(value.trim()).then(responce => console.log(responce));
+// }
+// }
